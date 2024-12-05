deeaf6e6114d168638fae91aab93481efb4a8467
somanshh <rahulsoni0530@gmail.com>
2024-12-05 15:37:07 +0530

    admin credentials

--- server/Controller/AdminController.js
+++ server/Controller/AdminController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const getCredentials = (req, res) => {
-    const Credentials = { "username": "admin", "password": "123" }
+    const Credentials = { "username": "admin", "password": "123" } // we can create more admin's here 
     res.status(200).json(Credentials)
 }
 
